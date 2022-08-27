2f3195c17a84b0fd7fe8c28f8c3a6da980056865
Mike Castano <castanomiguel1993@gmail.com>
2022-08-27 17:59:29 -0500

    Second commit

--- src/ejemplo.js
+++ src/ejemplo.js
@@ -3,8 +3,9 @@ let peliculas = [
   { numero: 2, genero: "humor", titulo: "risas" },
   { numero: 3, genero: "Suspenso", titulo: "No corras" },
   { numero: 4, genero: "Drama", titulo: "Amor mio" },
-];
 
+];
+console.log("otro cambio")
 let wrapMovies = document.getElementById("wrapMovies");
 let templateMovies = peliculas.forEach((item) => {
   wrapMovies.innerHTML += `<div class="movie"><p>${item.numero}</p><p>${item.genero}</p><p>${item.titulo}</p></div>`;
